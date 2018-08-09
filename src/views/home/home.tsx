11a7b9c3d0b11eda393b5280d0c0975c54060804
johnzhu12 <1042532367@qq.com>
2018-08-09 21:07:44 +0800

    update

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -7,19 +7,31 @@ import Header from '@components/header/header'
 import Menu from '@components/menu/menu'
 import Footer from '@components/footer/footer'
 
+interface HomeProps {
+
+}
+interface HomeStates {
+    name: string
+
+}
 @observer
-class HomePage extends React.Component<{}, {}>{
+class HomePage extends React.Component<HomeProps, HomeStates>{
     constructor(props) {
         super(props)
         // ['requestTest'].forEach(m => this[m] = this[m].bind(this)) //bind this for methods
+        this.state = {
+            name: ''
+        }
     }
     requestTest() {
         let params = {}
+        let that = this;
         deposit.ajax({
             url: '/test1/getNames',
             data: params,
             callback(data) {
-                console.log('我是请求的数据')
+                console.log('我是请求的数据', data)
+                that.setState({ name: data.name2 })
             }
         })
     }
@@ -30,6 +42,7 @@ class HomePage extends React.Component<{}, {}>{
                 <Menu />
                 <Button type='primary' onClick={this.requestTest.bind(this)}>
                     测试接口
+                    {this.state.name}
                 </Button>
                 <Footer />
             </div>
