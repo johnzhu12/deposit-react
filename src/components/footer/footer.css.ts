935cd97130072a158177758b3243d21f258de6bf
dayu <1342278185@qq.com>
2018-08-10 16:01:50 +0800

    footer部分及header变更

---
+++ src/components/footer/footer.css.ts
@@ -0,0 +1,75 @@
+import styled from 'styled-components'
+
+const FooterTag = styled.div`
+.footerFrame{
+    width:100%;
+    height:400px;
+    background-color:pink;
+    .footerContent{
+        /*background-color:green;*/
+        margin:0 120px;
+        /* min-width: 1000px;*/
+        width:100%;
+        height: 100%;
+      float:left;
+    font-size:0;
+    .right{
+        text-align:center;
+        width:25%;
+        display: inline-block;
+	    vertical-align: top;  
+    }
+    .middle{
+        text-align:center;
+        width:25%;
+        display: inline-block;
+	    vertical-align: top; 
+    }
+    .left{
+        text-align:center;
+        width:25%;
+        display: inline-block;
+    }
+    li{
+        text-align:left;
+        margin-bottom:10px;
+        font-size:14px;
+        font-family:'microsoft yahei';
+    }
+    ul>li:first-child{
+        margin-top:55px;
+        padding-bottom:30px;
+        font-weight:bold;
+        font-size:24px;
+    }
+    .qrCode{
+        width:25%;
+        float:right;
+        
+        .pp{
+            display:inline-block;
+            font-size:24px;
+            margin-top:50px;
+            color:rgb(237,126,30);
+            display:inline-block;
+            padding-right:15px;
+        }
+        .ppNumber{
+            font-family:Palatino Linotype;
+            font-size:34px;
+            color:rgb(237,126,30);
+            display:inline-block;
+        }
+        .ppName{
+            
+            font-size:24px;
+            font-family:'microsoft yahei';
+            color:rgb(138,141,150);
+        }
+
+    }
+}
+}
+`
+
+export default FooterTag;
